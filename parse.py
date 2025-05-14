f391430dabbf744fb7acb4c742f2eda23ebfb4dc
Pinguini456 <noahsnachos@gmail.com>
2025-05-14 14:46:27 +1200

    filling gaps and adding more block icons

--- parse.py
+++ parse.py
@@ -26,6 +26,85 @@ def _into_pyobj(tag: BaseTag) -> Any:
 
     return tag
 
+def correct_item_names(data):
+    name = ""
+    match data['Name']:
+        case "minecraft:banner":
+            match data['Damage']:
+                case 0:
+                    name = "minecraft:black_banner"
+                case 1:
+                    name = "minecraft:red_banner"
+                case 2:
+                    name = "minecraft:green_banner"
+                case 3:
+                    name = "minecraft:brown_banner"
+                case 4:
+                    name = "minecraft:blue_banner"
+                case 5:
+                    name = "minecraft:purple_banner"
+                case 6:
+                    name = "minecraft:cyan_banner"
+                case 7:
+                    name = "minecraft:light_gray_banner"
+                case 8:
+                    name = "minecraft:gray_banner"
+                case 9:
+                    name = "minecraft:pink_banner"
+                case 10:
+                    name = "minecraft:lime_banner"
+                case 11:
+                    name = "minecraft:yellow_banner"
+                case 12:
+                    name = "minecraft:light_blue_banner"
+                case 13:
+                    name = "minecraft:magenta_banner"
+                case 14:
+                    name = "minecraft:orange_banner"
+                case 15:
+                    name = "minecraft:white_banner"
+        case "minecraft:bed":
+            match data['Damage']:
+                case 0:
+                    name = "minecraft:white_bed"
+                case 1:
+                    name = "minecraft:orange_bed"
+                case 2:
+                    name = "minecraft:magenta_bed"
+                case 3:
+                    name = "minecraft:light_blue_bed"
+                case 4:
+                    name = "minecraft:yellow_bed"
+                case 5:
+                    name = "minecraft:lime_bed"
+                case 6:
+                    name = "minecraft:pink_bed"
+                case 7:
+                    name = "minecraft:gray_bed"
+                case 8:
+                    name = "minecraft:light_gray_bed"
+                case 9:
+                    name = "minecraft:cyan_bed"
+                case 10:
+                    name = "minecraft:purple_bed"
+                case 11:
+                    name = "minecraft:blue_bed"
+                case 12:
+                    name = "minecraft:brown_bed"
+                case 13:
+                    name = "minecraft:green_bed"
+                case 14:
+                    name = "minecraft:red_bed"
+                case 15:
+                    name = "minecraft:black_bed"
+        case "minecraft:undyed_shulker_box":
+            name = "minecraft:shulker_box"
+        case _:
+            name = data["Name"]
+
+    return name
+
+
 def beautify(struct: dict):
     new_struct = {}
     for key in struct.keys():
@@ -68,6 +147,7 @@ def total(indices, palette, data):
     for i in indices:
         name = palette[i][0]
         inc = 1
+        item_name = ""
 
         if str(count) in data:
 
@@ -78,11 +158,6 @@ def total(indices, palette, data):
                 unpowered_repeater
                 powered_repeater
                 lit_redstone_lamp
-                undyed_shulker_box - item
-                
-                also:
-                bed - item - damage
-                banner - item - damage
                 
             """
             match name:
@@ -191,69 +266,79 @@ def total(indices, palette, data):
                             name = "minecraft:white_banner"
                 case "minecraft:smoker":
                     for item in entity['block_entity_data']['Items']:
-                        if item['Name'] in blocks:
-                            blocks[item['Name']] += item['Count']
+                        item_name = correct_item_names(item)
+                        if item_name in blocks:
+                            blocks[item_name] += item['Count']
                         else:
-                            blocks[item['Name']] = item['Count']
+                            blocks[item_name] = item['Count']
                 case "minecraft:chest":
                     for item in entity['block_entity_data']['Items']:
-                        if item['Name'] in blocks:
-                            blocks[item['Name']] += item['Count']
+                        item_name = correct_item_names(item)
+                        if item_name in blocks:
+                            blocks[item_name] += item['Count']
                         else:
-                            blocks[item['Name']] = item['Count']
+                            blocks[item_name] = item['Count']
                 case "minecraft:barrel":
                     for item in entity['block_entity_data']['Items']:
-                        if item['Name'] in blocks:
-                            blocks[item['Name']] += item['Count']
+                        item_name = correct_item_names(item)
+                        if item_name in blocks:
+                            blocks[item_name] += item['Count']
                         else:
-                            blocks[item['Name']] = item['Count']
+                            blocks[item_name] = item['Count']
                 case str(x) if 'shulker' in x:
                     if name == 'minecraft:undyed_shulker_box':
                         name = "minecraft:shulker_box"
                     for item in entity['block_entity_data']['Items']:
-                        if item['Name'] in blocks:
-                            blocks[item['Name']] += item['Count']
+                        item_name = correct_item_names(item)
+                        if item_name in blocks:
+                            blocks[item_name] += item['Count']
                         else:
-                            blocks[item['Name']] = item['Count']
+                            blocks[item_name] = item['Count']
                 case "minecraft:glow_frame":
                     name = "minecraft:glow_item_frame"
                     if "Item" in entity['block_entity_data']:
-                        if entity['block_entity_data']["Item"]["Name"] in blocks:
-                            blocks[entity['block_entity_data']["Item"]["Name"]] += 1
+                        item_name = correct_item_names(entity['block_entity_data']['Item'])
+                        if item_name in blocks:
+                            blocks[item_name] += 1
                         else:
-                            blocks[entity['block_entity_data']["Item"]["Name"]] = 1
+                            blocks[item_name] = 1
                 case "minecraft:frame":
                     name = "minecraft:item_frame"
                     if "Item" in entity['block_entity_data']:
-                        if entity['block_entity_data']["Item"]["Name"] in blocks:
-                            blocks[entity['block_entity_data']["Item"]["Name"]] += 1
+                        item_name = correct_item_names(entity['block_entity_data']['Item'])
+                        if item_name in blocks:
+                            blocks[item_name] += 1
                         else:
-                            blocks[entity['block_entity_data']["Item"]["Name"]] = 1
+                            blocks[item_name] = 1
                 case "minecraft:dropper":
                     for item in entity['block_entity_data']['Items']:
-                        if item['Name'] in blocks:
-                            blocks[item['Name']] += item['Count']
+                        item_name = correct_item_names(item)
+                        if item_name in blocks:
+                            blocks[item_name] += item['Count']
                         else:
-                            blocks[item['Name']] = item['Count']
+                            blocks[item_name] = item['Count']
                 case "minecraft:hopper":
                     for item in entity['block_entity_data']['Items']:
-                        if item['Name'] in blocks:
-                            blocks[item['Name']] += item['Count']
+                        item_name = correct_item_names(item)
+                        if item_name in blocks:
+                            blocks[item_name] += item['Count']
                         else:
-                            blocks[item['Name']] = item['Count']
+                            blocks[item_name] = item['Count']
                 case "minecraft:chiseled_bookshelf":
                     if 'Items' in entity['block_entity_data']:
                         for item in entity['block_entity_data']['Items']:
-                            if item['Name'] in blocks:
-                                blocks[item['Name']] += item['Count']
+                            item_name = correct_item_names(item)
+                            if item_name in blocks:
+                                blocks[item_name] += item['Count']
                             else:
-                                blocks[item['Name']] = item['Count']
+                                blocks[item_name] = item['Count']
                 case "minecraft:furnace":
                     for item in entity['block_entity_data']['Items']:
-                        if item['Name'] in blocks:
-                            blocks[item['Name']] += item['Count']
+                        item_name = correct_item_names(item)
+                        if item_name in blocks:
+                            blocks[item_name] += item['Count']
                         else:
-                            blocks[item['Name']] = item['Count']
+                            blocks[item_name] = item['Count']
                 case "minecraft:lectern":
                     if "book" in entity['block_entity_data']:
                         if "minecraft:book_and_quill" in blocks:
@@ -262,22 +347,25 @@ def total(indices, palette, data):
                             blocks['minecraft:book_and_quill'] = 1
                 case "minecraft:dispenser":
                     for item in entity['block_entity_data']['Items']:
-                        if item['Name'] in blocks:
-                            blocks[item['Name']] += item['Count']
+                        item_name = correct_item_names(item)
+                        if item_name in blocks:
+                            blocks[item_name] += item['Count']
                         else:
-                            blocks[item['Name']] = item['Count']
+                            blocks[item_name] = item['Count']
                 case "minecraft:blast_furnace":
                     for item in entity['block_entity_data']['Items']:
-                        if item['Name'] in blocks:
-                            blocks[item['Name']] += item['Count']
+                        item_name = correct_item_names(item)
+                        if item_name in blocks:
+                            blocks[item_name] += item['Count']
                         else:
-                            blocks[item['Name']] = item['Count']
+                            blocks[item_name] = item['Count']
                 case "minecraft:trapped_chest":
                     for item in entity['block_entity_data']['Items']:
-                        if item['Name'] in blocks:
-                            blocks[item['Name']] += item['Count']
+                        item_name = correct_item_names(item)
+                        if item_name in blocks:
+                            blocks[item_name] += item['Count']
                         else:
-                            blocks[item['Name']] = item['Count']
+                            blocks[item_name] = item['Count']
 
 
         if name in blocks:
